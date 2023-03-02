9d5573133116ca44971adab64544b8679469fa0d
IgorRadtke <igor.radtke@hotmail.com>
2023-03-02 20:18:05 -0300

    Inclusão do XML e inicio da sintatica

--- src/lexica.py
+++ src/lexica.py
@@ -410,6 +410,7 @@ def main():
     
     
 if __name__ == '__main__':
+    import sintatico
     main()
     # TODO:
     # 1. Finalizar a implementação do analisador léxico
---
+++ src/sintatico.py
@@ -0,0 +1,7 @@
+import sys
+import pandas as pd
+import xml.etree.ElementTree as ET
+
+# ENTRADAS
+parsing = ET.parse('inputs/GoldParser.xml')
+root = parsing.getroot()
